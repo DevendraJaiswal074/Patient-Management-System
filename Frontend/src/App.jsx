d3ff7d22d88285dc150d8863282da19161cea5b0
Devendrajaiswal074 <devendrajaiswal074@gmail.com>
2026-01-26 19:16:43 +0530

    Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='h-screen w-full bg-gray-800'>
-      <p className='text-2xl py-4 px-10 bg-gray-900 font-bold text-white text-center'>Patient-Management-System</p>
+    <div className='h-screen w-full bg-blue-50'>
+      <Navbar />
     </div>
   )
 }
 
-export default App
+export default App;
