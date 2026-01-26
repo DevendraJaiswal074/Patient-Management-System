d17b3a07358045cf97f5bb2a0832d7e15ed0d509
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-26 20:34:07 +0530

    LoginOption Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import LoginOption from './Pages/LoginOption';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='h-screen w-full bg-blue-50'>
-      <Navbar />
+        {/* <Navbar /> */}
+      <LoginOption/>
     </div>
   )
 }
